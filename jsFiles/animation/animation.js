c611112059168b39ff89a435d282dac0a8c7742c
gingerjournal <linpeijiang6@gmail.com>
2026-07-03 18:41:56 -0400

    Init commit

---
+++ jsFiles/animation/animation.js
@@ -0,0 +1,15 @@
+// loading animation
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").classList.add("hidden");
+    }, 1000);
+});
+
+// when user refresh, move to very top
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.addEventListener("load", () => {
+    window.scrollTo(0, 0);
+});
---
+++ jsFiles/animation/layoverScroller.js
@@ -0,0 +1,40 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const pinnedCards = gsap.utils.toArray(".card.pinned");
+    const finalScrollCard = document.querySelector(".card.scroll");
+
+    pinnedCards.forEach((card, index) => {
+        const next = pinnedCards[index + 1] || finalScrollCard;
+
+        // 1. Pin the card
+        ScrollTrigger.create({
+            trigger: card,
+            start: "center center",
+            end: () => "+=" + next.offsetHeight,
+            pin: true,
+            pinSpacing: false,
+            scrub: 1,
+        });
+
+        // 2. Scale down as the next card approaches
+        gsap.to(card, {
+            scale: 0.7,
+            opacity: 0.9,
+            ease: "none",
+            scrollTrigger: {
+                trigger: next,
+                start: "top bottom",
+                end: "top top",
+                scrub: true,
+            }
+        });
+
+        // 3. hide card when it was covered
+        ScrollTrigger.create({
+            trigger: next,
+            start: "top top",
+            onEnter: () => card.style.display = "none",
+        });
+    });
+});
