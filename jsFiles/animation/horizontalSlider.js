18081971363876dd6a4419e1e3139a5e50aaaf3b
gingerjournal <linpeijiang6@gmail.com>
2026-08-06 02:43:15 -0400

    Home/index page finished for both desktop and mobile view.

--- jsFiles/animation/horizontalSlider.js
+++ jsFiles/animation/horizontalSlider.js
@@ -4,28 +4,18 @@ const texts = gsap.utils.toArray(".anim");
 const mask = document.querySelector(".mask");
 
 let scrollTween = gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".container",
-        pin: true,
-        scrub: 1,
-        end: "+=2000",
-        markers: false,
-    }
-});
+        xPercent: -300,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".container",
+            pin: true,
+            scrub: 1,
+            end: "+=2000",
+            markers: false,
+        }
+    });
 
 console.log(1 / (sections.length - 1))
-
-//Progress bar animation
-
-gsap.to(mask, {
-    width: "100%",
-    scrollTrigger: {
-        trigger: ".wrapper",
-        scrub: 1
-    }
-});
 
 // whizz around the sections
 sections.forEach((section) => {
--- jsFiles/other.js
+++ jsFiles/other.js
@@ -4,9 +4,13 @@ window.addEventListener("scroll", () => {
 });
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > window.innerHeight - 100) {
+    if (window.innerWidth > 1000 && window.scrollY > window.innerHeight - 100) {
         document.body.classList.add("header-fixed");
+    }
+    else if(window.innerWidth <= 1000 && window.scrollY > window.innerHeight - 150) {
+            document.body.classList.add("header-fixed-mobile");
     } else {
         document.body.classList.remove("header-fixed");
+        document.body.classList.remove("header-fixed-mobile");
     }
 });
