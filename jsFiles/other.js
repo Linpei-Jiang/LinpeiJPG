14f61b4b7f744f2bef51c8765d9c0a0810c41267
gingerjournal <linpeijiang6@gmail.com>
2026-08-06 02:49:39 -0400

    header bug for ipad fix + featured work grid fix? v1.

--- jsFiles/other.js
+++ jsFiles/other.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", () => {
     if (window.innerWidth > 1000 && window.scrollY > window.innerHeight - 100) {
         document.body.classList.add("header-fixed");
     }
-    else if(window.innerWidth <= 1000 && window.scrollY > window.innerHeight - 150) {
+    else if(window.innerWidth <= 1200 && window.scrollY > window.innerHeight - 150) {
             document.body.classList.add("header-fixed-mobile");
     } else {
         document.body.classList.remove("header-fixed");
